141971dfb148e55a58998563a40cf2f733961429
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-06-09 16:07:17 +0800

    Add files via upload

---
+++ wizard/purchase_order_cancel.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-abs
+from odoo import models
+
+
+class PurchaseOrderCancel(models.TransientModel):
+    _name = "purchase.order.cancel"
+    _description = "Purchase Orders Cancel"
+
+    def cancel_order(self):
+        purchase_orders = self.env['purchase.order'].browse(
+            self._context.get('active_ids', []))
+        purchase_orders.write({'state': 'cancel'})
+        purchase_orders.unlink()
+        # return purchase_orders
